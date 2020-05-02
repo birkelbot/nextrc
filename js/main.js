8aba2d387478ac4b2bfffa15ae72a557e74ec79d
Matt Birkel <mbirkel91@gmail.com>
2020-05-02 17:05:46 -0500

    Implemented the nav bar.

--- js/main.js
+++ js/main.js
@@ -3,18 +3,121 @@
   "use strict";
 
   $(function() {
+
+    /****************************************/
+    /***             SPINNER              ***/
+    /****************************************/
+
     // Remove the spinner when the page is loaded.
     $(window).on('load', function() {
       if ($('.spinner')) {
         $('.spinner').removeClass('show');
       }
-    })
+    });
+
+
+    /****************************************/
+    /***    DYNAMIC HOME SECTION HEIGHT   ***/
+    /****************************************/
 
     // Adjust height of the home section to fit the viewport.
     $('.home').css('height', $(window).height() - $('.navbar').height() - $('.home').next().height());
-    $(window).resize(function(){
+    $(window).resize(function() {
       $('.js-fullheight').css('height', $(window).height());
     });
+
+
+    /****************************************/
+    /***        HIDE/SHOW NAV BAR         ***/
+    /****************************************/
+
+    var prevScrollPos = Math.ceil(window.pageYOffset);
+    var forceHideNavBar = false;
+    var unlockNavBarPos = null;
+    var preventNavBarHiding = false;
+
+    function hideNavBar() {
+      if (!preventNavBarHiding) {
+        $('.navbar').css('top', -$('.navbar').outerHeight());
+      }
+    }
+
+    function showNavBar() {
+      $('.navbar').css('top', 0);
+    }
+
+    function isAtSetpoint(setpoint, actualPosition) {
+      if (setpoint - 1 <= actualPosition && actualPosition <= setpoint + 1) {
+        return true;
+      }
+      return false;
+    }
+
+    $('.nav-link').click(function() {
+      // Automatically collapse the nav bar.
+      if (!$('.navbar-toggler').hasClass('collapsed')) {
+        $('.navbar-toggler').click();
+      }
+
+      // We can't scroll below the bottom of the page.
+      var scrollSetpoint = Math.ceil(Math.min($($(this).data('href')).offset().top,        // Position of target
+                                              $(document).height() - $(window).height())); // Bottom of page
+      // Deduct height of the collapsible section of the nav bar, which hasn't 
+      // been collapsed yet, but is currently in the process of being collapsed.
+      // NOTE: It takes some time for Bootstrap to process the click (above), and we
+      //       don't want to wait for that to complete before starting scrolling.
+      scrollSetpoint -= $('.navbar-collapse').height();
+
+      // Hide the nav bar until reaching the target.
+      forceHideNavBar = true;
+      unlockNavBarPos = scrollSetpoint;
+
+      window.scrollTo(0, scrollSetpoint);
+    });
+
+    // Prevent hiding the nav bar when the nav bar is expanded.
+    $('.navbar-toggler').click(function() {
+      if ($(this).hasClass('collapsed')) {
+        preventNavBarHiding = true;
+      } else {
+        preventNavBarHiding = false;
+      }
+    });
+
+    // Hide/show the nav bar when scrolling.
+    window.onscroll = function() {
+      var currentScrollPos = Math.ceil(window.pageYOffset);
+      if (forceHideNavBar) {
+        // Keep the nav bar hidden until reaching the unlock position.
+        if (isAtSetpoint(unlockNavBarPos, currentScrollPos)) {
+          forceHideNavBar = false;
+        } else {
+          hideNavBar();
+        }
+      } else {
+        // After reaching the unlock position, don't actually unlock the nav bar
+        // until the user scrolls away from the unlock position.
+        if (unlockNavBarPos) {
+          if (!isAtSetpoint(unlockNavBarPos, currentScrollPos)) {
+            unlockNavBarPos = null;
+          }
+        }
+        // The nav bar is unlocked. Behave as normal.
+        else {
+          if (currentScrollPos > prevScrollPos) {
+            hideNavBar();
+          } else if (currentScrollPos < prevScrollPos) {
+            showNavBar();
+          }
+        }
+      }
+      prevScrollPos = currentScrollPos;
+    };
+
+
+    /****************************************/
+    /***       WAYPOINT ANIMATIONS        ***/
+    /****************************************/
 
     // Set up content waypoint animations.
     $('.animate').waypoint(function(direction) {
